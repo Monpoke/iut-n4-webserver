ebaf4d787caa120faf1b20406156259017f35269
Pierre Bourgeois <dragonralph@gmail.com>
2016-01-27 16:57:54 +0100

    Can reuse address

--- webserver/socket.c
+++ webserver/socket.c
@@ -4,23 +4,31 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 #include "socket.h"
 #include "configuration.h"
 
 int creer_serveur(int port) {
 
+
+
     int socket_serveur;
     socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
     if (socket_serveur == -1) {
         perror("socket_serveur");
         /*  traitement  de l’erreur  */
+        exit(1);
     }
-    
+
+    int optval = 1;
+    if (setsockopt(socket_serveur, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof (int)) == -1)
+        perror("Can not set  SO_REUSEADDR  option");
+
     /*  Utilisation  de la  socket  serveur  */
     struct sockaddr_in saddr;
     saddr.sin_family = AF_INET;
-    
+
     /*  Socket  ipv4 */
     saddr.sin_port = htons(port);
     /* Port d’écoute  */
