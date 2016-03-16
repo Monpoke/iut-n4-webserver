4765bc00be85d68be3535e0d36f510a6c38da539
Florent Marcaille <florent.marcaille@univ-lille1.fr>
2016-03-16 14:03:29 +0100

    cheking repository existing

--- webserver/main.c
+++ webserver/main.c
@@ -11,6 +11,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <dirent.h>
 
 #include "fileReader.h"
 #include "client.h"
@@ -102,7 +103,15 @@ char * open_documentroot(int argc, char** argv) {
         exit(1);
     }
     
-    char * document_root = argv[1];
+    DIR * dir = opendir(argv[1]);
+    char * document_root ;
+
+    if(dir != NULL){
+        document_root = argv[1];
+    }else{
+        perror("Repository error");
+        exit(1);
+    }
     
     if(open(document_root, O_RDONLY)==-1){
         perror("Document root");
@@ -121,8 +130,8 @@ int main(int argc, char** argv) {
 
     
     // Mimes
-    loadMimes();
-    exit(0);
+    //loadMimes();
+    //exit(0);
     
     // document root
     char* docroot = open_documentroot(argc, argv);
@@ -132,7 +141,7 @@ int main(int argc, char** argv) {
     printf("Server launched:\n");
 
     // Mimes
-    loadMimes();
+    //loadMimes();
     
     // Signaux
     initialiser_signaux();
